c7f673ee60692610ce4e36e34c12198dcb29afe1
Fábio simones <simones.fabio1994@gmail.com>
2025-01-03 14:26:55 -0300

    Resolução do desafio.

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,45 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.projections.ReportMinProjection;
+import com.devsuperior.dsmeta.projections.SummaryMinProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    // salesSummary = Retorna o nome do vendedor e a quantidade vendida para a respectiva data.
+    //SELECT SUM (TB_SALES.AMOUNT) AS TOTAL, TB_SELLER.NAME AS SELLERNAME FROM TB_SALES JOIN TB_SELLER ON TB_SALES.SELLER_ID = TB_SELLER.ID WHERE TB_SALES.DATE BETWEEN '2022-01-01' AND '2022-06-30' GROUP BY TB_SELLER.ID ORDER BY TB_SELLER.NAME
+    @Query(nativeQuery = true, value =
+            "SELECT SUM (TB_SALES.AMOUNT) AS TOTAL, " +
+            "TB_SELLER.NAME AS SELLERNAME " +
+            "FROM TB_SALES " +
+            "JOIN TB_SELLER ON TB_SALES.SELLER_ID = TB_SELLER.ID " +
+            "WHERE TB_SALES.DATE BETWEEN :minDate AND :maxDate " +
+            "GROUP BY TB_SELLER.ID " +
+            "ORDER BY TB_SELLER.NAME"
+    )
+    List<SummaryMinProjection> salesSummary(String minDate, String maxDate);
 
+    //salesReport = Retorna vendedores e suas vendas para o nome 'Odinson'.
+    //SELECT tb_sales.id, tb_sales.date, tb_sales.amount, tb_seller.name as sellerName
+    //FROM tb_sales JOIN tb_seller ON tb_sales.seller_id = tb_seller.id
+    //WHERE tb_sales.date BETWEEN '2022-05-01' AND '2022-05-31' AND UPPER(tb_seller.name) LIKE UPPER(CONCAT('%', 'odinson', '%'))
+    @Query(nativeQuery = true, value =
+            "SELECT tb_sales.id, tb_sales.date, tb_sales.amount, tb_seller.name as sellerName " +
+            "FROM tb_sales " +
+            "JOIN tb_seller ON tb_sales.seller_id = tb_seller.id " +
+            "WHERE tb_sales.date BETWEEN :minDate AND :maxDate " +
+            "AND UPPER(tb_seller.name) LIKE UPPER(CONCAT('%',:sellerName, '%'))",
+            countQuery = "SELECT COUNT(tb_sales.id) " +
+                    "FROM tb_sales " +
+                    "JOIN tb_seller ON tb_sales.seller_id = tb_seller.id " +
+                    "WHERE tb_sales.date BETWEEN :minDate AND :maxDate " +
+                    "AND UPPER(tb_seller.name) LIKE UPPER(CONCAT('%',:sellerName, '%'))"
+    )
+    Page<ReportMinProjection> salesReport(String minDate, String maxDate, String sellerName, Pageable pageable);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,21 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.*;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SaleService {
@@ -19,5 +27,55 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<SaleReportDTO> getReport(String minDate, String maxDate, String sellerName, Pageable pageable){
+		LocalDate[] dates = getDates(minDate, maxDate);
+
+		LocalDate finalDate = dates[0];
+		LocalDate startDate = dates[1];
+
+		return repository.salesReport(
+				startDate.toString(), finalDate.toString(), sellerName, pageable
+		).map(x -> new SaleReportDTO(x));
+
+
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSummaryDTO> getSummary(String minDate, String maxDate) {
+		LocalDate[] dates = getDates(minDate, maxDate);
+
+		LocalDate finalDate = dates[0];
+		LocalDate startDate = dates[1];
+
+		return repository.salesSummary(startDate.toString(), finalDate.toString())
+				.stream()
+				.map(x -> new SaleSummaryDTO(x)).toList();
+	}
+
+
+	//Instanciando datas para caso não forem informadas.
+	private LocalDate[] getDates(String minDate, String maxDate) {
+		LocalDate finalDate;
+		LocalDate startDate;
+
+		if(maxDate.isEmpty()){
+			finalDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+		else {
+			finalDate = LocalDate.parse(maxDate);
+		}
+
+		if(minDate.isEmpty()){
+			startDate = finalDate.minusYears(1L);
+		}
+		else{
+			startDate = LocalDate.parse(minDate);
+		}
+
+		return new LocalDate[]{finalDate, startDate};
+
 	}
 }
